b2a8714041705a5f33f3cb910dc15cda77bde34e
Hugh Wells <hugh@crablab.co.uk>
2020-06-28 14:28:28 +0100

    Add Pydoc

--- zevvle.py
+++ zevvle.py
@@ -2,11 +2,25 @@ import requests
 
 class zevvle():
 
-    def __init__(self, key, url):
+    def __init__(self, key, url="https://api.zevvle.com"):
+        """
+        Initializes the SDK.
+        
+        :param key: Your Zevvle API key. 
+        :param url optional: The Zevvle API URL. 
+        """
         self._url = url
         self._header = {"Authorization": "Bearer {}".format(key)}
     
     def _do_request(self, url, parameters={}):
+        """
+        Makes a request. 
+
+        :param url: The full URL of the request.
+        :param parameters optional: Dict of parameters for the request.
+
+        :returns: JSON response. 
+        """
         if(not url):
             raise Exception("Called without URL")
 
@@ -18,27 +32,60 @@ class zevvle():
         raise Exception("Request failed with error {}".format(str(r.status_code)))
 
     def get_account(self, account_id):
+        """
+        Looks up Zevvle account.
+        
+        :param account_id: ID of the Zevvle account to look up.
+        :returns: Zevvle account details. 
+        """
         if(not account_id):
             raise Exception("Missing account_id parameter")
         
         return self._do_request("/accounts/{}".format(account_id))
     
     def get_user(self, user_id):
+        """
+        Looks up Zevvle user.
+        
+        :param user_id: ID of the Zevvle user to look up.
+        :returns: Zevvle user details. 
+        """
         if(not user_id):
             raise Exception("Missing user_id parameter")
             
         return self._do_request("/users/{}".format(user_id))
 
     def get_sim(self, sim_id):
+        """
+        Looks up Zevvle SIM card.
+        
+        :param sim_id: ID of the Zevvle SIM card to look up.
+        :returns: Zevvle SIM card details. 
+        """
         if(not sim_id):
             raise Exception("Missing sim_id parameter")
             
         return self._do_request("/sim_cards/{}".format(sim_id))
 
     def list_sim_cards(self):
+        """
+        Lists all SIM cards linked to the Zevvle API key.
+        
+        :returns: SIM cards for the API key in use. 
+        """
         return self._do_request("/sim_cards")
 
     def list_call_records(self, sim_id, type="", limit="", before=None, after=None):
+        """
+        Lists call records for a given Zevvle SIM ID, according to filtering. 
+        
+        :param sim_id: ID of the Zevvle SIM card to get records for.
+        :param type optional: Call record type (data, voice, sms, mms) to filter on.
+        :param limit optional: How many records to limit the results to. 
+        :param before: Limit results to records before a given datetime. 
+        :param after: Limit results to records after a given datetime. 
+        :returns: Call records for the given query. 
+        """
         parameters = {}
 
         if(not sim_id):
@@ -64,6 +111,12 @@ class zevvle():
         return self._do_request("/call_records/", parameters)
      
     def get_call_record(self, call_record_id): 
+        """
+        Looks up Zevvle call record.
+        
+        :param call_record_id: ID of the Zevvle call record to look up.
+        :returns: Zevvle call record details. 
+        """
         if(not call_record_id):
             raise Exception("Missing call_record_id parameter")
         
